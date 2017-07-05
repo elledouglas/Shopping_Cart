cb856b685cdf6dd9f9fb9092d95f584e7a8a7f17
Kathleen <kathleenmcclay@gmail.com>
2017-07-05 09:36:37 -0400

    Tried tested and true

--- shoppingcart.rb
+++ shoppingcart.rb
@@ -6,40 +6,54 @@ class Shopping
 
   def initialize
     @products = []
-
+    @tax_rate = 3.3
   end
 
 
   def add_product(name, base_price)
     new_product = Products.new(name, base_price)
 @products << new_product
-puts new_product
+
   end
 
 
   def remove(product)
-    @products.delete(product)
+
+  @products.each do |product|
+    if product.name == product
+      product.delete
+
+end
+end
 end
 
 
 def total_cost_BEFORE_tax
-  tota_cost = 0
-@product.each do |product|
-  total_cost = total_cost + product.base_price
-  puts total_cost
+  total_cost = 0
+@products.each do |product|
+  total_cost += product.base_price
+
 end
+return total_cost
 end
 
 def total_cost_AFTER_tax
 total_cost = 0
 @products.each do |product|
-  total_cost = total_cost + (product.base_price * tax_rate)
-  puts total_cost
+  total_cost = total_cost + (product.base_price * @tax_rate)
 end
-end
-
-
+return total_cost
 end
 
-puts Kellogs = Products.new("Kellogs", 5)
-puts Kellogs.inspect
+
+end
+puts basket = Shopping.new
+puts kellogs = Products.new("Kellogs", 5)
+puts basket.add_product("Kellogs", 5)
+puts basket.remove("Kellogs")
+puts cereal = Products.new("fff", 5)
+puts basket.total_cost_BEFORE_tax
+puts basket.total_cost_AFTER_tax
+
+#puts kellogs.delete("Kellogs")
+#puts soap = Products.new("Soap", 5)
